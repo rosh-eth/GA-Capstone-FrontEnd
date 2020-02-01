f0cdcee827f901e2b0c436ddcfa0737ba4285d2f
RGhadamian <roshan@ghadamian.com>
2020-02-01 16:40:18 +1100

    added a signup form

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form, Button, Card } from 'react-bootstrap'
+
+const SignUpForm = () => {
+
+    return (
+        <div>
+            <Card bg="dark" text="white" style={{ width: '24rem' }} className="mx-auto d-flex align-items-center">
+            <Card.Img className="img-thumbnail" style={{ width: '8rem' }} variant="top" src="https://instadapp.io/img/logo.png" />
+            <Card.Header>InstaDapp</Card.Header>
+                <Card.Body>
+                    <Card.Title className="text-center">Login</Card.Title>
+                    <Card.Text>
+                        <Form className="text-center" >
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                    </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Check me out" />
+                            </Form.Group>
+                            <Button variant="light" type="submit">
+                                Login
+                </Button >
+                        </Form>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+
+
+            </div>
+    )
+}
+
+export default SignUpForm;
+
