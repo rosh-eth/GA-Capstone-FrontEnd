478da5c8393a2e0dc6f7005a1b182a7e529b5176
RGhadamian <roshan@ghadamian.com>
2020-02-01 14:41:30 +1100

    added font awesome package and added icons and text for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {Jumbotron, Button} from 'react-bootstrap';
+import {Jumbotron, Button, Row, Container} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee , faChevronRight} from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
 
     return (
         <React.Fragment>
-            <Jumbotron>
-                <h1>Hello, world!</h1>
-                <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
+            <Container>
+                <Row>
+                    <Button>InstaDapp</Button>
+                    <Button className="float-right">Dashboard</Button>
+                </Row>
+            </Container>
+            <Jumbotron className="text-center">
+                <h1>Trustless <span className="text-primary">Smart Wallet</span> for DeFi.</h1>
+                <p className="text-secondary">
+                Seamlessly manage, optimize and deploy your assets to get the best returns across protocols.
   </p>
                 <p>
-                    <Button variant="primary">Learn more</Button>
+                    <Button variant="primary">Dashboard <FontAwesomeIcon icon={faChevronRight} /></Button>
+                    <Button variant="primary">Newsletter</Button>
                 </p>
             </Jumbotron>
         </React.Fragment>
