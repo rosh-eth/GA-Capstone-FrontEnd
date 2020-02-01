5eefe3523d4334f24d673701e115e5adbe85de32
RGhadamian <roshan@ghadamian.com>
2020-02-01 14:03:19 +1100

    Added Jumbotron to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 const Home = () => {
 
-return (
-    <React.Fragment>
-        <p>Hello</p>
-    </React.Fragment>
-)
+    return (
+        <React.Fragment>
+            <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+  </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
+        </React.Fragment>
+    )
 }
 
 export default Home;
