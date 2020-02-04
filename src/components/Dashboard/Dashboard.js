e740071464f1e4badee804bb5f9110be9139863f
RGhadamian <roshan@ghadamian.com>
2020-02-04 20:00:42 +1100

    Renamed Menu to Dashboard menu and moved to utils

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './Menu';
 import { Row, Col } from 'react-bootstrap';
 import Title from './Title';
 import MakerVault from './MakerVault';
 import CompoundPosition from './CompoundPosition';
 import MakerCDP from './MakerCDP';
 import Balance from './Balance';
+import DashboardMenu from '../Utils/DashboardMenu';
 
 const Dashboard = () => {
  
@@ -13,7 +13,7 @@ const Dashboard = () => {
         <React.Fragment>
             <Row>
                 <Col xs="2" sm="2" md="4" lg="3" xl="3">
-                    <Menu />
+                    <DashboardMenu />
                 </Col>
                 <Col xs="10" sm="10" md="8" lg="9" xl="9">
                     <Title />
