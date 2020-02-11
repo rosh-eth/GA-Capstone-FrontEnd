4db32506481fd15f01ba1075199757dac22da3b3
RGhadamian <roshan@ghadamian.com>
2020-02-11 20:54:03 +1100

    changed styling for statement component

--- src/components/Home/Statement.js
+++ src/components/Home/Statement.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Jumbotron, Button, Row, Container, Image, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const Statement = () => {
 
+    const style = {
+        'background-image': "url('https://instadapp.io/img/illustrations/illustration-3-cropped.png'), url('https://instadapp.io/img/illustrations/illustration-1-cropped.png')",
+        'background-repeat': 'no-repeat'
+    }
+
     return (
         <div>
-            <Jumbotron className="text-center my-5 py-5">
-            {/* style={{ background-image: url('https://instadapp.io/img/illustrations/illustration-1-cropped.png'}} */}
-                <h2>DeFi is complex. Just Use InstaDApp</h2>
-                <h4 className="text-secondary font-weight-light py-3">
-                    Seamlessly manage, optimize and deploy your assets to get the best returns across protocols.
-                </h4>
+            <Jumbotron className="text-center my-5 py-5" style={style}>
+                <h3 className="font-weight-normal">DeFi is complex. Just Use InstaDApp</h3>
+                <h5 className="text-muted font-weight-light py-3 ">
+                We can help you manage your wealth better on blockchain.
+                </h5>
 
-                <Link to="/dashboard"><Button variant="primary mr-3 ">Dashboard <FontAwesomeIcon icon={faChevronRight} /></Button></Link>
-                <Button variant="secondary">Newsletter</Button>
+                <Link to="/dashboard"><Button variant="primary mr-3 ">Get Started</Button></Link>
+
             </Jumbotron>
             
         </div>
