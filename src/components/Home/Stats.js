bde8aa966543450c768aca8f88209415b422ac10
RGhadamian <roshan@ghadamian.com>
2020-02-11 19:57:16 +1100

    created a stats component on homepage

---
+++ src/components/Home/Stats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Jumbotron, Button, Row, Container, Image, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const Stats = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default Stats
