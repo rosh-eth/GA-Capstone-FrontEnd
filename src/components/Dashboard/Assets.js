1a40e1a790fd34978bd8e7e5c99fe48e5f342097
RGhadamian <roshan@ghadamian.com>
2020-02-13 20:26:43 +1100

    Added debt & networth to assets

--- src/components/Dashboard/Assets.js
+++ src/components/Dashboard/Assets.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import { Card , Col, Row, CardColumns} from 'react-bootstrap';
-import {calcAssets } from './Calculations/Calculations';
+import {netWorth} from './Calculations/Calculations';
 import {UserContext} from '../../App';
 
 const Assets = () => {
@@ -15,7 +15,8 @@ const Assets = () => {
                         <Card.Body>
                             <Card.Title>Assets</Card.Title>
                             <Card.Text>
-                                <h2>${calcAssets(userContext.ethPrice, userContext.eth, userContext.daiPrice, userContext.dai, userContext.batPrice, userContext.bat)}</h2>
+                                <h2>${userContext.assetBalance}</h2>
+                                
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -23,7 +24,7 @@ const Assets = () => {
                         <Card.Body>
                             <Card.Title>Net Worth</Card.Title>
                             <Card.Text>
-                                <h2>$2,000.00</h2>
+                                <h2>${netWorth(userContext.assetBalance, userContext.debt)} </h2>
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -31,7 +32,7 @@ const Assets = () => {
                         <Card.Body>
                             <Card.Title>Debt</Card.Title>
                             <Card.Text>
-                                <h2>$1,000.00</h2>
+                                <h2>${userContext.debt}</h2>
                             </Card.Text>
                         </Card.Body>
                     </Card>
--- src/components/Dashboard/Calculations/Calculations.js
+++ src/components/Dashboard/Calculations/Calculations.js
@@ -8,10 +8,17 @@ export function calcAssets(ethPrice, ethBalance, daiPrice, daiBalance, batPrice,
         const ethTotal = calcBalance(ethPrice, ethBalance);
         const daiTotal = calcBalance(daiPrice, daiBalance);
         const batTotal = calcBalance(batPrice, batBalance);
+        console.log('ethAssets', ethTotal)
+        console.log('daiAssets', daiTotal)
+        console.log('batAssets', batTotal)
 
-        const totalAssets = parseFloat(batTotal) + parseFloat(daiTotal) + parseFloat(ethTotal);
+        const totalAssets = parseFloat(batTotal) + parseFloat(daiTotal) + parseFloat(ethTotal); 
+        console.log('totalAssets', totalAssets)
 
         return totalAssets;
 }
 
-
+export function netWorth(Assets, Debt) {
+        const calc = parseFloat(Assets-Debt).toFixed(2);
+        return calc
+}
