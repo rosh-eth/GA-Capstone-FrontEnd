3dfb4fd3b4edf3050fcb9ba1daa7607fa3a7422f
RGhadamian <roshan@ghadamian.com>
2020-02-13 20:26:34 +1100

    Refactored Assets into App State

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import {Switch, Route} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
+import {calcAssets } from './components/Dashboard/Calculations/Calculations';
 
 const UserContext = React.createContext();
 const PortfolioProvider = UserContext.Provider;
@@ -21,19 +22,21 @@ const App = () => {
   const [eth, setEth] = useState(0.00);
   const [dai, setDai] = useState(0.00);
   const [bat, setBat] = useState(0.00);
+  const [debt, setDebt] = useState(0.00);
   const [ethPrice, setEthPrice] = useState(0);
   const [batPrice, setBatPrice] = useState(0);
   const [daiPrice, setDaiPrice] = useState(0);
-  
+  const [assetBalance, setAssetBalance] = useState(0);
   
   useEffect(() => {
     fetch(`http://localhost:4000/auth/profile/${userId}`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      setEth(data.user.assets.eth)
-      setDai(data.user.assets.dai)
-      setBat(data.user.assets.bat)
+      setEth(data.user.assets.eth);
+      setDai(data.user.assets.dai);
+      setBat(data.user.assets.bat);
+      setDebt(data.user.debt);
     })
   }, [])
 
@@ -41,32 +44,39 @@ const App = () => {
     console.log('updated state');
   }, [eth, dai, bat])
 
-  useEffect(() => {
-    fetch(`http://localhost:4000/price/eth`)
+  useEffect(async () => {
+    await fetch(`http://localhost:4000/price/eth`)
     .then(response => response.json())
     .then(data => {
       setEthPrice(data.usd_price);
     })
 
-    fetch(`http://localhost:4000/price/bat`)
+    await fetch(`http://localhost:4000/price/bat`)
     .then(response => response.json())
     .then(data => {
       setBatPrice(data.usd_price);
     })
 
-    fetch(`http://localhost:4000/price/dai`)
+    await fetch(`http://localhost:4000/price/dai`)
     .then(response => response.json())
     .then(data => {
       setDaiPrice(data.usd_price);
     })
+    
   }, [])
+
+  useEffect(() => {
+    console.log('assetBalance', assetBalance)
+    setAssetBalance(calcAssets(ethPrice, eth, daiPrice, dai, batPrice, bat));
+    console.log('assetBalance after', assetBalance)
+  })
 
   AOS.init()
   library.add(fab)
   
   return (
     <div>
-      <PortfolioProvider value={{userId, setUser, eth, setEth, dai, setDai, bat, setBat, ethPrice, setEthPrice, batPrice, setBatPrice, daiPrice, setDaiPrice}}>
+      <PortfolioProvider value={{userId, setUser, eth, setEth, dai, setDai, bat, setBat, ethPrice, setEthPrice, batPrice, setBatPrice, daiPrice, setDaiPrice, assetBalance, setAssetBalance, debt, setDebt}}>
       <Switch>
       <Container style={{ background: '#F9FBFD' }} fluid >
         <Route exact path="/" component={Home} />
